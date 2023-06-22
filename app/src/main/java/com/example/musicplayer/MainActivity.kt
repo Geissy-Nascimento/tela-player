32adbf3d7ed7f02671f50a7936c07490a2c24a71
Geissy <geissy.kelly.nascimento08@gmail.com>
2023-06-22 15:51:12 -0300

    tudo funcionando sempre

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -2,32 +2,28 @@ package com.example.musicplayer
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.View
-import androidx.activity.ComponentActivity
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mediaplayer = MediaPlayer.create(this,R.raw.music)
+        var play_btn: ImageButton = findViewById<ImageButton>(R.id.play_btn)
+
+        val mediaplayer = MediaPlayer.create(this, R.raw.music)
 
         play_btn.setOnClickListener {
 
             if (mediaplayer.isPlaying){
                 mediaplayer.start()
-
                 play_btn.setImageResource(R.drawable.baseline_pause_24)
-                }else{
-                    mediaplayer.pause()
-                    play_btn.setImageResource(R.drawable.baseline_play_arrow_24)
-
-
+            }else{
+                mediaplayer.pause()
+                play_btn.setImageResource(R.drawable.baseline_play_arrow_24)
             }
         }
     }
